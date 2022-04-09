4fca9542050d34c47b8914e97711a077365f996e
JNUYoung <you@example.com>
2022-04-09 20:26:11 +0800

    modification

--- code/26.py
+++ code/26.py
@@ -1,18 +1,16 @@
-def removeDuplicates(nums):
+class Solution(object):
+    def removeDuplicates(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
         length = len(nums)
-        left = 0
-        right = 0
-
-        if length == 0:
-            return 0 
-        if length == 1:
-            return 1
-        if length == 2:
-            return 1 if nums[0] == nums[-1] else 2
+        left = 1
+        right = 1
+        # left指针和right指针从第二个元素开始，这是一个关键的地方
+        # 让右指针移动的同时，判断其所指元素和上一个元素是否相等，因为数组是升序排列
+        # 因此出现连续两个元素不相等时，将右指针覆盖到左指针，再移动左指针；
+        # 若元素一直连续相等，则移动右指针到不等元素 
 
         while right<length:
             if nums[right] != nums[right-1]:
@@ -20,5 +18,3 @@ def removeDuplicates(nums):
                 left = left + 1
             right = right + 1 
         return left
-
-print(removeDuplicates([1,1]))
